e3cfd8a5fd9ef3bfbad34754d9e8bda59bcfd081
王含泽 <whzcorcd@gmail.com>
2018-01-29 18:19:08 +0800

    fixed a bug that from the lastest updating

--- sw.js
+++ sw.js
@@ -32,8 +32,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "corcd.xyz",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
